42bde8d6ea65dfb8eaa617ecf1f235687029351d
João Victor Ramos <devjoaoramos@gmail.com>
2025-11-25 23:28:39 -0300

    feat: init commit

---
+++ src/test/java/br/com/ucsal/disponibilidadeprofessores/DisponibilidadeProfessoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ucsal.disponibilidadeprofessores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisponibilidadeProfessoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
